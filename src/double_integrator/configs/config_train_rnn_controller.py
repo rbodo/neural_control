ce382aaeb184c42952115829c3776e12c60c4b6e
rbodo <rbodo@mailbox.org>
2022-08-19 01:15:54 +0200

    Implemented pipeline to sweep perturbations. Gramians not yet working.

--- src/double_integrator/configs/config_train_rnn_controller.py
+++ src/double_integrator/configs/config_train_rnn_controller.py
@@ -1,5 +1,8 @@
 import os
 
+import numpy as np
+from yacs.config import CfgNode
+
 from src.double_integrator import configs
 
 
@@ -9,13 +12,24 @@ def get_config(timestamp_workdir=None):
     base_path = \
         '/home/bodrue/Data/neural_control/double_integrator/rnn_controller'
     config.paths.BASE_PATH = base_path
-    config.paths.PATH_FIGURES = os.path.join(base_path, 'figures')
-    config.paths.FILEPATH_MODEL = os.path.join(base_path, 'models',
-                                               'rnn.params')
+    config.paths.FILEPATH_MODEL = os.path.join(base_path, 'rnn.params')
     config.paths.FILEPATH_INPUT_DATA = \
         os.path.abspath(os.path.join(base_path, '..', 'lqg_grid.pkl'))
 
     config.process.PROCESS_NOISES = config.process.PROCESS_NOISES[:1]
-    config.process.OBSERVATION_NOISES = config.process.OBSERVATION_NOISES[:2]
+    config.process.OBSERVATION_NOISES = config.process.OBSERVATION_NOISES[:1]
+
+    config.model.NUM_HIDDEN_NEURALSYSTEM = 50
+    config.model.NUM_LAYERS_NEURALSYSTEM = 1
+    config.model.NUM_HIDDEN_CONTROLLER = 40
+    config.model.NUM_LAYERS_CONTROLLER = 1
+    config.model.REGULARIZATION_LEVELS = [0]
+        # np.logspace(-5, -2, 3, dtype='float32').tolist()
+    config.perturbation = CfgNode()
+    config.perturbation.PERTURBATION_TYPES = \
+        ['sensor', 'actuator', 'processor']
+    config.perturbation.PERTURBATION_LEVELS = \
+        np.logspace(-2, -1, 3, dtype='float32').tolist()
+    config.perturbation.DELTA = 1e-5
 
     return config
--- src/double_integrator/train_rnn_controller.py
+++ src/double_integrator/train_rnn_controller.py
@@ -2,10 +2,10 @@ import logging
 import os
 import sys
 import time
-from itertools import product
 
 import mlflow
 import mxnet as mx
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from mxnet import autograd
@@ -14,34 +14,86 @@ from tqdm.contrib import tenumerate
 
 from src.double_integrator import configs
 from src.double_integrator.control_systems import RNNModel
-from src.double_integrator.plotting import plot_training_curve
+from src.double_integrator.emgr import emgr
 from src.double_integrator.utils import get_artifact_path, get_data_loaders
+from src.ff_pid.brownian import brownian
 
 
-class CombinedRNN(mx.gluon.HybridBlock):
+def plot_control_output(output, label):
+    plt.close()
+    fig = plt.figure()
+    plt.plot(output[0, 0].asnumpy(), label='RNN')
+    plt.plot(label[0, 0].asnumpy(), label='LQR')
+    plt.legend()
+    plt.xlabel('Time')
+    plt.ylabel('Control')
+    return fig
 
-    def __init__(self, model: RNNModel, controller: RNNModel, **kwargs):
 
+class ControlledNeuralSystem(mx.gluon.HybridBlock):
+
+    def __init__(self, neuralsystem: RNNModel, controller: RNNModel, **kwargs):
         super().__init__(**kwargs)
-
-        self.model = model
+        self.neuralsystem = neuralsystem
         self.controller = controller
 
     def hybrid_forward(self, F, x, *args):
-        model_hidden, controller_hidden = args
-        model_outputs = []
-        for model_input in x:
+        neuralsystem_hidden, controller_hidden = args
+        neuralsystem_outputs = []
+        for neuralsystem_input in x:
             if F is mx.ndarray:
-                model_input = F.expand_dims(model_input, 0)
+                neuralsystem_input = F.expand_dims(neuralsystem_input, 0)
             controller_output, controller_hidden = self.controller(
-                model_hidden[0], controller_hidden)
-            model_output, model_hidden = self.model(
-                model_input, [model_hidden[0] + controller_output])
-            model_outputs.append(model_output)
-        return F.concat(*model_outputs, dim=0)
+                neuralsystem_hidden[0], controller_hidden)
+            neuralsystem_output, neuralsystem_hidden = self.neuralsystem(
+                neuralsystem_input, [neuralsystem_hidden[0]+controller_output])
+            neuralsystem_outputs.append(neuralsystem_output)
+        return F.concat(*neuralsystem_outputs, dim=0)
+
+    def begin_state(self, batch_size, ctx):
+        return (self.neuralsystem.rnn.begin_state(batch_size, ctx=ctx),
+                self.controller.rnn.begin_state(batch_size, ctx=ctx))
+
+    def apply_drift(self, where, dt, delta, drift, rng):
+        if where in (None, 'None', 'none', ''):
+            return
+        elif where == 'sensor':
+            parameters = self.neuralsystem.rnn.l0_i2h_weight
+        elif where == 'processor':
+            parameters = self.neuralsystem.rnn.l0_h2h_weight
+        elif where == 'actuator':
+            parameters = self.neuralsystem.decoder.weight
+        else:
+            raise NotImplementedError
+        shape = parameters.shape
+        w = np.ravel(parameters.data().asnumpy())
+        w = brownian(w, 1, dt, delta, drift, None, rng)[0]
+        parameters.data()[:] = np.reshape(w, shape)
+
+    def get_reg_weights(self):
+        return [self.controller.decoder.weight,
+                self.controller.rnn.l0_i2h_weight]
 
 
-def evaluate(model, test_data_loader, loss_function, init, context):
+class L2L1(mx.gluon.loss.L2Loss):
+    def __init__(self, lambda_: float, context, **kwargs):
+        super().__init__(**kwargs)
+        self.l1 = mx.gluon.loss.L1Loss(lambda_)
+        self.context = context
+
+    def hybrid_forward(self, F, pred, label, weight_list: list = None,
+                       sample_weight=None):
+        l2 = super().hybrid_forward(F, pred, label, sample_weight)
+        if weight_list is not None:
+            for weights in weight_list:
+                l2 = l2 + self.l1(
+                    weights.data(), F.zeros(weights.shape,
+                                            ctx=self.context)).mean()
+        return l2
+
+
+def evaluate(model, test_data_loader, init, context):
+    loss_function = mx.gluon.loss.L2Loss()
     validation_loss = 0
     for data, label in test_data_loader:
         data = mx.nd.moveaxis(data, -1, 0)
@@ -54,145 +106,218 @@ def evaluate(model, test_data_loader, loss_function, init, context):
     return validation_loss
 
 
-def train_single(config, plot_control=True, plot_loss=True, save_model=True):
-    context = mx.gpu(GPU) if mx.context.num_gpus() > 0 else mx.cpu()
-    os.environ['MXNET_CUDNN_LIB_CHECKING'] = '0'
-
-    model_num_hidden = config.model.NUM_HIDDEN
-    model_num_layers = config.model.NUM_LAYERS
-    controller_num_hidden = 40
-    controller_num_layers = 1
+def get_model(config, context, freeze_neuralsystem, freeze_controller,
+              load_weights_from: str = None):
+    neuralsystem_num_hidden = config.model.NUM_HIDDEN_NEURALSYSTEM
+    neuralsystem_num_layers = config.model.NUM_LAYERS_NEURALSYSTEM
+    controller_num_hidden = config.model.NUM_HIDDEN_CONTROLLER
+    controller_num_layers = config.model.NUM_LAYERS_CONTROLLER
+    activation = config.model.ACTIVATION
     num_inputs = 1
     num_outputs = 1
-    activation = config.model.ACTIVATION
-    path_data = config.paths.FILEPATH_INPUT_DATA
-    batch_size = config.training.BATCH_SIZE
-    lr = config.training.LEARNING_RATE
-    num_epochs = config.training.NUM_EPOCHS
-    optimizer = config.training.OPTIMIZER
 
-    data = pd.read_pickle(path_data)
-
-    test_data_loader, train_data_loader = get_data_loaders(data, config,
-                                                           'observations')
-
-    model = RNNModel(model_num_hidden, model_num_layers, num_outputs,
-                     num_inputs, activation, prefix='model_')
+    neuralsystem = RNNModel(neuralsystem_num_hidden, neuralsystem_num_layers,
+                            num_outputs, num_inputs, activation,
+                            prefix='neuralsystem_')
+    if freeze_neuralsystem:
+        neuralsystem.collect_params().setattr('grad_req', 'null')
 
     controller = RNNModel(controller_num_hidden, controller_num_layers,
-                          model_num_hidden, model_num_hidden, activation,
-                          prefix='controller_')
+                          neuralsystem_num_hidden, neuralsystem_num_hidden,
+                          activation, prefix='controller_')
+    if freeze_controller:
+        controller.collect_params().setattr('grad_req', 'null')
 
-    combined = CombinedRNN(model, controller)
-    combined.initialize(mx.init.Xavier(), context)
-    combined.hybridize(active=True, static_alloc=True, static_shape=True)
-    # controller.collect_params().setattr('grad_req', 'null')
+    model = ControlledNeuralSystem(neuralsystem, controller)
+    model.initialize(mx.init.Xavier(), context)
+    model.hybridize(active=True, static_alloc=True, static_shape=True)
 
-    loss_function = mx.gluon.loss.L2Loss()
-    trainer = mx.gluon.Trainer(combined.collect_params(), optimizer,
-                               {'learning_rate': lr,
-                                'rescale_grad': 1 / batch_size})
+    if load_weights_from is not None:
+        model.load_parameters(load_weights_from, ctx=context)
 
-    model_hidden = model.rnn.begin_state(batch_size, ctx=context)
-    controller_hidden = controller.rnn.begin_state(batch_size, ctx=context)
-    init = (model_hidden, controller_hidden)
-    training_losses = []
-    validation_losses = []
-    for epoch in trange(num_epochs, desc='epoch'):
+    return model
+
+
+# noinspection PyUnusedLocal
+class Gramians:
+    def __init__(self, model: ControlledNeuralSystem, context, dt, T):
+        self.model = model
+        self.context = context
+        self.dt = dt
+        self.T = T
+        self.num_steps = 1
+        self.batch_size = 1
+        self.num_inputs = model.neuralsystem.num_hidden
+        self.num_layers = model.neuralsystem.num_layers
+        self.num_hidden = model.neuralsystem.num_hidden
+        self.num_outputs = model.neuralsystem.num_hidden
+
+    def h(self, x, u):
+        u0 = mx.nd.zeros((self.num_steps, self.batch_size, self.num_inputs),
+                         ctx=self.context)
+        u0[0, 0, :] = u
+        x0 = mx.nd.zeros((self.num_layers, self.batch_size, self.num_hidden),
+                         ctx=self.context)
+        x0[:, 0, :] = x
+        y, x1 = self.model.controller.rnn(u0, [x0])
+        return y, x1[0]
+
+    def f(self, x, u, p, t):
+        _, x1 = self.h(x, u)
+        return x1.asnumpy()[0, 0]
+
+    def g(self, x, u, p, t):
+        y, _ = self.h(x, u)
+        z = self.model.controller.decoder(y)
+        return z.asnumpy()[0]
+
+    def compute_gramian(self, kind):
+        return emgr(self.f, self.g,
+                    [self.num_inputs, self.num_hidden, self.num_outputs],
+                    [self.dt, self.T], kind)
+
+    def compute_controllability(self):
+        return self.compute_gramian('c')
+
+    def compute_observability(self):
+        return self.compute_gramian('o')
+
+
+def train(config, perturbation_type, perturbation_level, regularization_level,
+          data_train, data_test, context, plot_control=True, save_model=True):
+
+    T = config.simulation.T
+    num_steps = config.simulation.NUM_STEPS
+    dt = T / num_steps
+    delta = config.perturbation.DELTA
+    batch_size = config.training.BATCH_SIZE
+    path_model = config.paths.FILEPATH_MODEL
+
+    model = get_model(config, context, freeze_neuralsystem=True,
+                      freeze_controller=False, load_weights_from=path_model)
+
+    if perturbation_level > 0:
+        model.apply_drift(perturbation_type, dt, delta, perturbation_level,
+                          RNG)
+
+    if regularization_level > 0:
+        loss_function = L2L1(regularization_level, context)
+    else:
+        loss_function = mx.gluon.loss.L2Loss()
+    trainer = mx.gluon.Trainer(
+        model.collect_params(), config.training.OPTIMIZER,
+        {'learning_rate': config.training.LEARNING_RATE,
+         'rescale_grad': 1 / batch_size})
+
+    init = model.begin_state(batch_size, context)
+
+    baseline = evaluate(model, data_test, init, context) / len(data_test)
+    logging.info(f'Baseline perturbed and uncontrolled: {baseline:.5f}')
+
+    training_loss_mean = validation_loss_mean = None
+    for epoch in trange(config.training.NUM_EPOCHS, desc='epoch'):
         training_loss = 0
         label = None
         tic = time.time()
-        for batch_idx, (data, label) in tenumerate(train_data_loader,
-                                                   desc='batch', leave=False):
+        for batch_idx, (data, label) in tenumerate(data_train, desc='batch',
+                                                   leave=False):
             # Move time axis from last to first position to conform to RNN
             # convention.
             data = mx.nd.moveaxis(data, -1, 0)
             data = data.as_in_context(context)
             label = label.as_in_context(context)
             with autograd.record():
-                output = combined(data, *init)
+                output = model(data, *init)
                 output = mx.nd.moveaxis(output, 0, -1)
-                loss = loss_function(output, label)
-
+                if regularization_level > 0:
+                    loss = loss_function(output, label,
+                                         model.get_reg_weights())
+                else:
+                    loss = loss_function(output, label)
             loss.backward()
-
             trainer.step(batch_size)
-
             training_loss += loss.mean().asscalar()
 
-        training_loss_mean = training_loss / len(train_data_loader)
-        training_losses.append(training_loss_mean)
-
-        if plot_control:
-            fig = plt.figure()
-            plt.plot(output[0, 0].asnumpy(), label='RNN')
-            plt.plot(label[0, 0].asnumpy(), label='LQR')
-            plt.legend()
-            plt.xlabel('Time')
-            plt.ylabel('Control')
-            mlflow.log_figure(fig, f'figures/control_{epoch}.png')
-            plt.close(fig)
-
-        validation_loss = evaluate(combined, test_data_loader, loss_function,
-                                   init, context)
-        validation_loss_mean = validation_loss / len(test_data_loader)
-        validation_losses.append(validation_loss_mean)
+        training_loss_mean = training_loss / len(data_train)
+        validation_loss = evaluate(model, data_test, init, context)
+        validation_loss_mean = validation_loss / len(data_test)
         logging.debug("Epoch {:3} ({:2.1f} s): loss {:.3e}, val loss {:.3e}."
                       "".format(epoch, time.time() - tic,
                                 training_loss_mean, validation_loss_mean))
-        mlflow.log_metric('mean_validation_loss', validation_loss_mean, epoch)
         mlflow.log_metric('mean_training_loss', training_loss_mean, epoch)
+        mlflow.log_metric('mean_validation_loss', validation_loss_mean, epoch)
 
-    if plot_loss:
-        f = plot_training_curve(training_losses, validation_losses, show=False)
-        mlflow.log_figure(f, os.path.join('figures', 'training_curve.png'))
+        if plot_control:
+            f = plot_control_output(output, label)
+            mlflow.log_figure(f, f'figures/control_{epoch}.png')
 
     if save_model:
         path_model = get_artifact_path('models/rnn.params')
         os.makedirs(os.path.dirname(path_model), exist_ok=True)
-        combined.save_parameters(path_model)
+        model.save_parameters(path_model)
         logging.info(f"Saved model to {path_model}.")
 
-    return training_losses, validation_losses
+    # gramians = Gramians(model, context, dt, T)
+    g_c = None#gramians.compute_controllability()
+    g_o = None#gramians.compute_observability()
+
+    return {'perturbation_type': perturbation_type,
+            'perturbation_level': perturbation_level,
+            'regularization_level': regularization_level,
+            'training_loss': training_loss_mean,
+            'validation_loss': validation_loss_mean,
+            'controllability': g_c, 'observability': g_o}
 
 
-def train_sweep(config):
-    process_noises = config.process.PROCESS_NOISES
-    observation_noises = config.process.OBSERVATION_NOISES
+def main(config):
+    context = mx.gpu(GPU) if mx.context.num_gpus() > 0 else mx.cpu()
+    os.environ['MXNET_CUDNN_LIB_CHECKING'] = '0'
+    perturbation_types = config.perturbation.PERTURBATION_TYPES
+    perturbation_levels = config.perturbation.PERTURBATION_LEVELS
+    regularization_levels = config.model.REGULARIZATION_LEVELS
+    path_data = config.paths.FILEPATH_INPUT_DATA
+    data = pd.read_pickle(path_data)
+    data_test, data_train = get_data_loaders(data, config, 'observations')
 
     dfs = []
-    config.defrost()
-    mlflow.set_tracking_uri(
-        os.path.join('file:' + config.paths.BASE_PATH, 'mlruns'))
+    mlflow.set_tracking_uri(os.path.join('file:' + config.paths.BASE_PATH,
+                                         'mlruns'))
     mlflow.set_experiment('train_rnn_controller')
-    mlflow.start_run(run_name='Noise sweep parent')
+    mlflow.start_run(run_name='Main')
     with open(get_artifact_path('config.txt'), 'w') as f:
         f.write(_config.dump())
-    for w, v in tqdm(product(process_noises, observation_noises), 'noise',
-                     leave=False):
-        mlflow.start_run(run_name='Noise sweep child', nested=True)
-        config.process.PROCESS_NOISES = [w]
-        config.process.OBSERVATION_NOISES = [v]
-
-        t_loss, v_loss = train_single(config, plot_control=True)
-
-        dfs.append(pd.DataFrame({'process_noise': w,
-                                 'observation_noise': v,
-                                 'training_loss': t_loss,
-                                 'validation_loss': v_loss}))
-        mlflow.log_params({'process_noise': w, 'observation_noise': v})
+    for perturbation_type in tqdm(perturbation_types, 'perturbation_type',
+                                  leave=False):
+        mlflow.start_run(run_name='Perturbation type', nested=True)
+        mlflow.log_param('perturbation_type', perturbation_type)
+        for perturbation_level in tqdm(perturbation_levels,
+                                       'perturbation_level', leave=False):
+            mlflow.start_run(run_name='Perturbation level', nested=True)
+            mlflow.log_param('perturbation_level', perturbation_level)
+            for regularization_level in tqdm(regularization_levels,
+                                             'regularization_level',
+                                             leave=False):
+                mlflow.start_run(run_name='Regularization level', nested=True)
+                mlflow.log_param('regularization_level', regularization_level)
+                out = train(config, perturbation_type, perturbation_level,
+                            regularization_level, data_train, data_test,
+                            context)
+                dfs.append(out)
+                mlflow.end_run()
+            mlflow.end_run()
         mlflow.end_run()
-
-    df = pd.concat(dfs, ignore_index=True)
+    df = pd.DataFrame(dfs)
     df.to_pickle(get_artifact_path('output.pkl'))
+    mlflow.end_run()
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.WARN)
+    logging.basicConfig(level=logging.INFO)
     GPU = 2
+    RNG = np.random.default_rng(42)
 
     _config = configs.config_train_rnn_controller.get_config()
 
-    train_sweep(_config)
+    main(_config)
 
     sys.exit()
