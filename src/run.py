2f87f64e0370dd40fb829078b63924696b81d380
rbodo <rbodo@mailbox.org>
2022-09-30 16:08:01 +0200

    Run script now accepts filename as argument.

--- src/run.py
+++ src/run.py
@@ -4,16 +4,17 @@ import sys
 
 import mlflow
 
-label = 'nonlinear_rnn_rl'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('label', type=str)
+parser.add_argument('--sweep_id', type=int, required=False, default=-1)
+label = parser.parse_args().label
+sweep_id = parser.parse_args().sweep_id
 
 # Needs to be set before executing run function.
 # https://github.com/mlflow/mlflow/issues/608
 os.environ['MLFLOW_TRACKING_URI'] = 'file:' + os.path.expanduser(
     f'~/Data/neural_control/{label}/mlruns')
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--sweep_id', type=int, required=False, default=-1)
-sweep_id = parser.parse_args().sweep_id
 
 mlflow.run('https://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@github.com/rbodo/'
            'neural_control.git',
